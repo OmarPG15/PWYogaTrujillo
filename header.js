64fc9172407c4d26301f897e662cfbb1cb03bb0d
Alexander Garcia <alexandergarciasanchez9@gmail.com>
2024-10-12 23:43:50 -0500

    Tienda Cursos Virtuales

--- header.js
+++ header.js
@@ -45,5 +45,12 @@ window.addEventListener('DOMContentLoaded', ()=>{
     });
 })
 
+const btnCart = document.querySelector('.container-icon')
+const containerCartProducts = document.querySelector('.container-cart-products')
+
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart')
+})
+
 
 
